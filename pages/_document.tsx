8bd4954b4fd504a7951e9006594f19f13f6e7b61
Vivek Sundaray <viveksundaray123@outlook.com>
2023-01-04 23:18:02 +0530

    initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head>
+				<link rel="icon" type="image/x-icon" href="/next-favicon.png" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage, GetStaticProps } from 'next'
+// import styles from '../styles/Home.module.css'
+
+export const getStaticProps: GetStaticProps = async() => {
+	return {
+		props: {message: "Hello World"}
+	}
+}
+
+const Home: NextPage<{message: string}> = ({message}) => {
+	return <div className="container">
+		{message}
+	</div>
+}
+
+export default Home;
