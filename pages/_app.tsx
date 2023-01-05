37928ac0d86efef1073e26f8e55823ea529d5365
Vivek Sundaray <viveksundaray123@outlook.com>
2023-01-05 23:11:35 +0530

    formatted the files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from "next/head"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-	  <>
-		  <Head>
-			  <title>NextJS App</title>
-		  </Head>
-		  <Component {...pageProps} />
-	  </>
-  )
+	return (
+		<>
+			<Head>
+				<title>NextJS App</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
-import { NextPage, GetStaticProps } from 'next'
+import { NextPage, GetStaticProps } from "next";
 // import styles from '../styles/Home.module.css'
 
-export const getStaticProps: GetStaticProps = async() => {
+export const getStaticProps: GetStaticProps = async () => {
 	return {
-		props: {message: "Hello World"}
-	}
-}
+		props: { message: "Hello World" },
+	};
+};
 
-const Home: NextPage<{message: string}> = ({message}) => {
-	return <div className="container">
-		{message}
-	</div>
-}
+const Home: NextPage<{ message: string }> = ({ message }) => {
+	return <div className="container">{message}</div>;
+};
 
 export default Home;
